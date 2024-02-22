27b7b7de292f8d8a713a70850069b41a9384023d
TacticalNuclearRaccoon <deniz.pekin@protonmail.com>
2024-02-22 10:33:11 +0100

    dev: animation with transition()

--- 01-affluent_seine/main.js
+++ 01-affluent_seine/main.js
@@ -20,20 +20,30 @@ const source = [
 ];
 
 const height = 1.8;
+const duration = 1000;
 
 d3.select("div.content")
   .selectAll("div.name")
   .data(source)
   .join("div")
   .classed("name", true)
-  .style("transform", (d, i) => `translate(0,${i * height}em)`)
-  .text((d) => d[0]);
+  .style("transform", (d, i) => `translate(0, ${i * height}em)`)
+  .style("opacity", 0)
+  .text((d) => d[0])
+  .transition()
+  .duration(duration)
+  .style("opacity", 1);
 
 d3.select("div.content")
   .selectAll("div.bar")
   .data(source)
   .join("div")
   .classed("bar", true)
+  .style("width", 0)
+  .style("transform", (d, i) => `translate(10.5em, ${i * height}em)`)
+  .text((d) => d[1])
+  .style("opacity", 0)
+  .transition()
+  .duration(duration)
   .style("width", (d) => `${d[1] / 16}em`)
-  .style("transform", (d, i) => `translate(10.5em, ${i * height}em`)
-  .text((d) => d[1]);
+  .style("opacity", 1);
